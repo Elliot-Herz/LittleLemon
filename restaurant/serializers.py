28a1c5c1a0697a3dd8fc5999e80b6876705dd473
Elliot <elliot.herzig@gmail.com>
2023-11-07 08:25:09 -0800

    Second API exervise (booking API): https://www.coursera.org/learn/back-end-developer-capstone/supplement/2l9zS/exercise-set-up-the-table-booking-api

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Menu
+from .models import Menu, Booking
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +11,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+    
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import Menu
-from .serializers import MenuSerializer
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def sayHello(request):
@@ -19,3 +20,7 @@ class MenuItemView(ListCreateAPIView):
 class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
